21d9447830f32a3fbcd8bb670d879b72b74ca47a
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-04 23:11:32 +0900

    missing import

--- django-web/mysite/polls/views.py
+++ django-web/mysite/polls/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):

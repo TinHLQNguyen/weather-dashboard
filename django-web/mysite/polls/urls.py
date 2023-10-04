6f2f6d5285716cab549321cc056f2d956fcd0b83
TinHLQNguyen <tinnguyenhuulequang@gmail.com>
2023-10-04 21:49:49 +0900

    add namespace to polls

--- django-web/mysite/polls/urls.py
+++ django-web/mysite/polls/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# setting application namespace
+app_name = "polls"
 urlpatterns = [
     # ex: /polls/
     path("", views.index, name="index"),
